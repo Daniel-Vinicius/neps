74baad06b46975e34692335082856ebe886040ee
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-07-10 18:02:40 -0300

    isPrime

---
+++ logic/exercises/isPrime/isPrime.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  long long int n = 0;
+  cin >> n;
+
+  int numbersToTest[6] = {2, 3, 5, 7, 9, 11};
+  int moduleObtained;
+
+  if (n == 2 || n == 3 || n == 5 || n == 7 || n == 9 || n == 11)
+  {
+    cout << 'S' << endl;
+    return 0;
+  }
+  else if (n == 1)
+  {
+    cout << 'N' << endl;
+    return 0;
+  }
+
+  for (int i = 0; i < 6; i++)
+  {
+    moduleObtained = n % numbersToTest[i];
+    if (moduleObtained == 0)
+    {
+      cout << 'N' << endl;
+      return 0;
+    }
+  }
+
+  cout << 'N' << endl;
+  return 0;
+}
