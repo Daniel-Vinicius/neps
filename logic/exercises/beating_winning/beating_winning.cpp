44fc764bf80ec5fd5eacc73b51dac2b8d5e63e36
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-07-11 16:40:11 -0300

    correct

--- logic/exercises/beating_winning/beating_winning.cpp
+++ logic/exercises/beating_winning/beating_winning.cpp
@@ -2,26 +2,39 @@
 
 using namespace std;
 
-// did not passed 36 minutes
+int n, d;
+string numero_apresentador, maior_premio;
 
 int main()
 {
-  int N, D;
-  string n;
-  while (cin >> N >> D >> n && N != 0 && D != 0)
+  while (cin >> n >> d) // Enquanto houver coisas pra ler
   {
-    vector<char> numbersSorted;
-    for (int i = 0; i < n.size(); i++)
-    {
-      numbersSorted.push_back(n[i]);
-    }
-    sort(numbersSorted.begin(), numbersSorted.end());
+    if (n == 0 && d == 0)
+      return 0;
 
-    for (int j = 0; j < D; j++)
+    cin >> numero_apresentador;
+
+    int erased = 0;
+    for (char cur : numero_apresentador)
     {
-      n.erase(n.find(numbersSorted[j]), 1);
+      // algoritmo guloso
+      // O algoritmo guloso é apropriado quando uma solução ótima pode ser construída por meio de escolhas locais ótimas em cada etapa.
+      // O problema deve ter a propriedade de subestrutura ótima, o que significa que uma solução global ótima pode ser alcançada combinando soluções ótimas para subproblemas menores.
+      while (maior_premio.size() > 0 && cur > maior_premio.back() && erased < d)
+      {
+        maior_premio.pop_back();
+        erased++;
+      }
+
+      // n - d é o tamanho da resposta
+      if (maior_premio.size() < n - d)
+        maior_premio.push_back(cur); // append
     }
-    cout << n << endl;
+
+    cout << maior_premio << "\n";
+
+    numero_apresentador.clear();
+    maior_premio.clear();
   }
 
   return 0;
