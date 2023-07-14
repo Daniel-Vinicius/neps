b7b2b0986113845bc15226dce99c5992f88d3fd4
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-07-14 19:10:27 -0300

    ortografia

--- techniques/_exercises/orthography/orthography.cpp
+++ techniques/_exercises/orthography/orthography.cpp
@@ -1,7 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
-
 // Retirar uma letra de A.
 // Adicionar uma letra a A, em qualquer posição.
 // Trocar qualquer letra de A por outra letra, na mesma posição.
@@ -12,44 +12,52 @@ using namespace std;
 // Exemplos:
 // A palavra ‘tu’ pode se referir à palavra do dicionário ‘tubo’, realizando duas vezes a operação 2.
 
-// para cada palavra digitada verifica se
-// tamanho da palavra digitada - LCS dela com a palavra do dicionário[i] <= 2
-// answer[i].push(palavraDicionario)
+string palavrasDicionario[1001], palavrasDigitadas[101];
 
-int lcs(char *x, char *y, int n, int m, vector<vector<int>> &dp)
+// linhas -> o tamanho da palavra do dicionário
+// colunas -> deslocamento da palavra digitada em relação à palavra do dicionário.
+int distanceTable[22][6], n, m; // matriz bidimensional
+
+// semelhante a lcs
+int processWord(string &a, string &b)
 {
-  if (m == 0 || n == 0)
-    return 0;
+  int sa = a.size(), sb = b.size();
+  for (int i = 0; i <= 2; i++)
+    distanceTable[0][i + 2] = i;
 
-  if (x[n - 1] == y[m - 1])
-    return dp[n][m] = 1 + lcs(x, y, n - 1, m - 1, dp);
-
-  if (dp[n][m] != -1)
-    return dp[n][m];
-
-  return dp[n][m] = max(lcs(x, y, n, m - 1, dp), lcs(x, y, n - 1, m, dp));
-}
-
-void processa(string dicionario, string digitada, int dicionarioStrLen, int digitadaStrLen, vector<vector<string>> &ans, int dp_i)
-{
-  char x[dicionarioStrLen];
-  for (int i = 0; i < dicionarioStrLen; i++)
-    x[i] = dicionario.at(i);
-
-  char y[digitadaStrLen];
-  for (int i = 0; i < digitadaStrLen; i++)
-    y[i] = digitada.at(i);
-
-  vector<vector<int>> dp(dicionarioStrLen + 1, vector<int>(digitadaStrLen + 1, -1));
-  int commonSequence = lcs(x, y, dicionarioStrLen, digitadaStrLen, dp);
-
-  // cout << "dicionario: " << dicionario << " ";
-  // cout << digitadaStrLen << " " << commonSequence << " " << digitadaStrLen - commonSequence << endl;
-
-  if (digitadaStrLen - commonSequence < 2)
+  for (int i = 1; i <= sa; i++)
   {
-    ans[dp_i].push_back(dicionario);
+    for (int j = -2; j <= 2; j++)
+    {
+      if (i + j < 0 && i + j > sb)
+        continue;
+      if (i + j == 0)
+      {
+        distanceTable[i][j + 2] = i;
+        continue;
+      }
+
+      if (a[i - 1] == b[i + j - 1])
+        distanceTable[i][j + 2] = distanceTable[i - 1][j + 2];
+      else
+      {
+        distanceTable[i][j + 2] = distanceTable[i - 1][j + 2] + 1;
+        if (j != 2)
+          distanceTable[i][j + 2] = min(distanceTable[i][j + 2], distanceTable[i - 1][j + 3] + 1);
+        if (j != -2)
+          distanceTable[i][j + 2] = min(distanceTable[i][j + 2], distanceTable[i][j + 1] + 1);
+      }
+    }
   }
+
+  int distance = 10;
+  for (int i = -2; i <= 2; i++)
+  {
+    if (sa + i < 0 || sa + i > sb)
+      continue;
+    distance = distanceTable[sa][i + 2] + sb - sa - i;
+  }
+  return (distance < 3);
 }
 
 int main()
@@ -57,43 +65,22 @@ int main()
 
   // N -> qtd dicionario
   // M -> qtd digitada
-  int N, M;
-  cin >> N >> M;
-  vector<string> palavrasDicionario;
-  vector<string> palavrasDigitadas;
-  vector<vector<string>> ans(M, vector<string>(N));
+  cin >> n >> m;
 
-  for (int i = 0; i < N; i++)
+  for (int i = 0; i < n; i++)
+    cin >> palavrasDicionario[i];
+  for (int j = 0; j < m; j++)
+    cin >> palavrasDigitadas[j];
+
+  for (int j = 0; j < m; j++)
   {
-    string palavra;
-    cin >> palavra;
-    palavrasDicionario.push_back(palavra);
-  }
-
-  for (int i = 0; i < M; i++)
-  {
-    string palavra;
-    cin >> palavra;
-    palavrasDigitadas.push_back(palavra);
-
-    for (int j = 0; j < N; j++)
-      processa(palavrasDicionario[j], palavra, palavrasDicionario[j].length(), palavra.length(), ans, i);
-  }
-
-  for (int i = 0; i < ans.size(); i++)
-  {
-    for (int j = 0; j < ans[i].size(); j++)
+    for (int i = 0; i < n; i++)
     {
-      cout << ans[i][j] << " ";
+      if (processWord(palavrasDicionario[i], palavrasDigitadas[j]))
+        cout << palavrasDicionario[i] << " ";
     }
     cout << endl;
   }
 
   return 0;
 }
-
-// Output:
-
-// pato
-// (espaço em branco)
-// pato pateta
