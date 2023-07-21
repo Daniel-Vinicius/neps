ace70faa0fb77750ea4114ee48222ca8b790aadf
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-07-20 22:33:03 -0300

    toll working

--- graphs/_exercises/toll/toll.cpp
+++ graphs/_exercises/toll/toll.cpp
@@ -1,75 +1,64 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-const int MAXN = 2500;
+const int MAX_CITIES = 51;
+int num_cities, num_roads, starting_city, max_tolls;
+int dist[MAX_CITIES], queue[MAX_CITIES], front_queue, back_queue;
+bool adj[MAX_CITIES][MAX_CITIES];
 
 int main()
 {
-  int n_cities, n_roads, start_city, max_toll; // As cidades são identificadas por inteiros de 1 a n_cities
-  int conjunto = 0;
-  while (cin >> n_cities >> n_roads >> start_city >> max_toll && n_cities != 0 && n_roads != 0)
+  int city1, city2, test_num = 1, i, front;
+  while (cin >> num_cities >> num_roads >> starting_city >> max_tolls && num_cities > 0)
   {
-    conjunto++;
-
-    vector<int> citiesGraph[MAXN];
-    memset(citiesGraph, 0, sizeof citiesGraph);
-    for (int i = 1; i <= n_roads; i++)
+    // Inicializa a matriz de adjacência com valores falsos (sem conexões entre cidades)
+    memset(adj, false, sizeof(adj));
+    while (num_roads--)
     {
-      int x, y;
-      cin >> x >> y;
-      citiesGraph[x].push_back(y);
-      citiesGraph[y].push_back(x);
+      cin >> city1 >> city2;
+      // Marca as conexões bidirecionais entre cidades na matriz de adjacência
+      adj[city1][city2] = adj[city2][city1] = true;
     }
-
-    int layer[MAXN];
-    bool visited[MAXN];
-
-    for (int i = 0; i < n_cities; i++)
+    // Inicializa o vetor de distâncias com -1 (valor padrão para cidades não visitadas)
+    memset(dist, -1, sizeof(dist));
+    // Define a distância da cidade inicial para ela mesma como 0
+    dist[starting_city] = 0;
+    queue[0] = starting_city;
+    front_queue = 0;
+    back_queue = 1;
+    while (front_queue < back_queue && dist[queue[front_queue]] < max_tolls)
     {
-      visited[i] = false;
-      layer[i] = 0;
-    }
-
-    int l = max_toll;
-
-    queue<int> q;
-    q.push(start_city);
-    visited[start_city] = true;
-
-    while (!q.empty() && l >= 0)
-    {
-      int current = q.front();
-      q.pop();
-      l--;
-
-      for (int i = 0; i < citiesGraph[current].size(); i++)
+      front = queue[front_queue];
+      // Percorre todas as cidades para verificar as conexões com a cidade atual (front)
+      for (i = 1; i <= num_cities; i++)
       {
-        int neighbour = citiesGraph[current][i];
-
-        if (!visited[neighbour])
+        // Verifica se existe uma conexão direta (estrada) entre a cidade atual (front) e a cidade i
+        // E se a cidade i ainda não foi visitada (dist[i] == -1)
+        if (adj[front][i] && dist[i] == -1)
         {
-          q.push(neighbour);
-          visited[neighbour] = true;
-          layer[neighbour] = layer[current] + 1;
+          // Se atende aos critérios, adiciona a cidade i à fila para visitar posteriormente
+          // e atualiza a distância para a cidade i a partir da cidade inicial (front)
+          queue[back_queue++] = i;
+          dist[i] = dist[front] + 1;
         }
       }
+      // Move para a próxima cidade na fila
+      front_queue++;
     }
-
-    // encontre cada cidade (que não a cidade onde eles estão) que possa ser visitada por eles, dada a restrição de que o Sr. Juca deseja pagar no máximo
-    // P pedágios (considerando apenas a viagem de ida).
-
-    cout << "Teste " << conjunto << endl;
-
-    for (int i = 1; i <= n_cities; i++)
+    // Imprime o número do teste atual
+    cout << "Teste " << test_num++ << endl;
+    // Imprime todas as cidades (exceto a cidade inicial) que podem ser visitadas,
+    // respeitando o limite de pedágios estabelecido pelo Sr. Juca (max_tolls)
+    for (i = 1; i <= num_cities; i++)
     {
-      // cout << i << " layer[i]: " << layer[i] << endl;
-      if (layer[i] > 0 && layer[i] <= max_toll)
+      if (i != starting_city && dist[i] != -1)
+      {
         cout << i << " ";
+      }
     }
-
-    cout << endl;
-    cout << endl;
+    cout << endl
+         << endl;
   }
 
   return 0;
